cc04922b24246bfd572571ebb3f65e14a96a15bf
Damian <38847935+DamianLokic@users.noreply.github.com>
2023-05-21 23:52:58 +0200

    Update calkowanie_metoda_simpsona.py

--- calkowanie_metoda_simpsona.py
+++ calkowanie_metoda_simpsona.py
@@ -3,15 +3,15 @@ import math
 a = 1.4
 b = 1.8
 n = 6
-h = (b - a) / (n-1)
-x = [a + i * h for i in range(n + 1)]
-y = [math.sin((x[i] / 2) + 0.1) / (2.2 + math.sqrt(0.7 * math.pow(x[i], 2) + 1.4)) for i in range(n + 1)]
+h = (b - a) / (2 * n)
+x = [a + i * h for i in range(2 * n)]
+y = [math.sin((x[i] / 2) + 0.1) / (2.2 + math.sqrt(0.7 * math.pow(x[i], 2) + 1.4)) for i in range(2 * n)]
 result = (y[0] + y[n - 1])
 
-for i in range(1, n - 1, 2):
+for i in range(1, 2 * n , 2):
     result += 4 * y[i]
 
-for i in range(2, n - 2, 2):
+for i in range(2, 2 * n - 1, 2):
     result += 2 * y[i]
 
 print((h / 3) * result)
